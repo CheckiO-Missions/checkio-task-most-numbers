28880815d61cbac0f893fc1f9ca0c47daf34fb57
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-02-04 11:15:55 +0400

    tests

--- generate_MN.py
+++ generate_MN.py
@@ -9,9 +9,10 @@ def solver(*args):
 tests = []
 
 for _ in range(20):
-    t = [randint(-100, 100) / (10 ** randint(0, 3)) for _ in range(randint(1, 20))]
+    t = [randint(-100, 0) / (10 ** randint(0, 3)) for _ in range(randint(1, 20))]
     ans = round(solver(*t), 4)
-    print('{{"input": {0},\n"answer": {1}\n}},\n'.format(t, ans))
+    print('{{"input": {0},\n"answer": {1},\n"explanation": "{2}-{3}"}},\n'.format(
+        t, ans, max(t), min(t) if min(t) >= 0 else "({0})".format(min(t))))
 
 
 
